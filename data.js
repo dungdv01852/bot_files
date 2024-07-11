cdc39c4dc5a58d3aa824edff73757c04a16e7448
dungdv01852 <135571721+dungdv01852@users.noreply.github.com>
2024-07-11 13:11:22 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,3 @@
+export const botAnswer = "Thanks you for you message. Please type '#chatbot' or '#ai' to chat with your virtual assitant"
+
+export const botUrl = "https://zerochat.us/embedded/chat?c=eyJ0aGVtZSI6ImxpZ2h0IiwiYWdlbnRJZCI6ImhNSVBnSGVMZVUyY1A1ZUpwODl1YXciLCJhZ2VudE5vZGVBZGRyZXNzIjoiaHR0cHM6Ly9hZ25vZGUxLnplcm9jaGF0LnVzIn0%3D"
